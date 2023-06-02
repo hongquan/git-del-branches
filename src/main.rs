3aeb2190be7ec796794a6ee91620be309b09c000
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2023-06-02 12:23:56 +0700

    More explanation

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,70 @@
-
-use eyre::Context;
-use git2::{Repository, BranchType, Branch};
-use inquire::{MultiSelect, Confirm};
-use inquire::error::InquireError;
 use color_eyre::Result;
 use console::{style, Emoji};
-
+use eyre::Context;
+use git2::{Branch, BranchType, Repository};
+use inquire::error::InquireError;
+use inquire::{Confirm, MultiSelect};
 
 const EXCLUDES: &[&str] = &["master", "main", "develop", "development"];
 
-
 fn get_branches(repo: &Repository, names: Vec<String>) -> Vec<Branch> {
-    names.into_iter().filter_map(|n| {
-        repo.find_branch(&n, BranchType::Local).ok()
-    }).collect::<Vec<Branch>>()
+    names
+        .into_iter()
+        .filter_map(|n| repo.find_branch(&n, BranchType::Local).ok())
+        .collect::<Vec<Branch>>()
 }
 
 fn show_list_of_branches(branch_pairs: &Vec<(Branch, Option<Branch>)>) {
-    let lines: Vec<String> = branch_pairs.iter().filter_map(|(lb, rb)| {
-        let local_name = lb.name().ok()??;
-        let upstream_name = rb.as_ref().map(|b| b.name().ok()).flatten().flatten();
-        let line = match upstream_name {
-            Some(name) => format!(" {local_name} ({name})"),
-            None => format!(" {local_name}")
-        };
-        Some(line)
-    }).collect();
+    let lines: Vec<String> = branch_pairs
+        .iter()
+        .filter_map(|(lb, rb)| {
+            let local_name = lb.name().ok()??;
+            let upstream_name = rb.as_ref().map(|b| b.name().ok()).flatten().flatten();
+            let line = match upstream_name {
+                Some(name) => format!(" {local_name} ({name})"),
+                None => format!(" {local_name}"),
+            };
+            Some(line)
+        })
+        .collect();
     eprintln!("{}", lines.join("\n"));
 }
 
-
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     color_eyre::install()?;
     let repo = Repository::discover(".").wrap_err("Not a Git working folder")?;
     let branches = repo.branches(Some(BranchType::Local))?;
-    let names: Vec<String> = branches.filter_map(|b| {
-        let branch = b.ok().map(|x| x.0)?;
-        let n = branch.name().ok()??;
-        if branch.is_head() || EXCLUDES.contains(&n) {
-            None
-        } else {
-            Some(n.to_string())
-        }
-    }).collect();
+    let staying_in_branch = repo.head().ok().map(|r| r.is_branch()).unwrap_or(false);
+    let names: Vec<String> = branches
+        .filter_map(|b| {
+            let branch = b.ok().map(|x| x.0)?;
+            let n = branch.name().ok()??;
+            if branch.is_head() || EXCLUDES.contains(&n) {
+                None
+            } else {
+                Some(n.to_string())
+            }
+        })
+        .collect();
     if names.is_empty() {
-        eprintln!("No branches found");
+        eprintln!("No branches eligible to delete.");
+        if staying_in_branch {
+            eprintln!("{}", style("You can not delete the branch to are staying in. Please switch to another one.").yellow());
+        }
         return Ok(());
     }
     let ans_branches = match MultiSelect::new("Select branches to delete", names).prompt() {
         Ok(ans) => ans,
         Err(InquireError::OperationCanceled) => return Ok(()),
-        Err(e) => return Err(e.into())
+        Err(e) => return Err(e.into()),
     };
-    let ans_up = match Confirm::new("Do you want to delete the upstream branches also").with_default(false).prompt() {
+    let ans_up = match Confirm::new("Do you want to delete the upstream branches also")
+        .with_default(false)
+        .prompt()
+    {
         Ok(ans) => ans,
         Err(InquireError::OperationCanceled) => return Ok(()),
-        Err(e) => return Err(e.into())
+        Err(e) => return Err(e.into()),
     };
     let msg = if ans_up {
         "To delete these branches and their upstream:"
@@ -64,18 +73,21 @@ fn main() -> Result<()>{
     };
     eprintln!("{}", style(msg).blue());
     let local_branches = get_branches(&repo, ans_branches);
-    let branch_pairs: Vec<(Branch, Option<Branch>)> = local_branches.into_iter().map(|b| {
-        let upstream = b.upstream().ok();
-        (b, upstream)
-    }).collect();
+    let branch_pairs: Vec<(Branch, Option<Branch>)> = local_branches
+        .into_iter()
+        .map(|b| {
+            let upstream = b.upstream().ok();
+            (b, upstream)
+        })
+        .collect();
     show_list_of_branches(&branch_pairs);
     for (mut lb, mut rb) in branch_pairs {
         lb.delete().ok();
         match rb.as_mut() {
             Some(b) => b.delete().ok(),
-            None => Some(())
+            None => Some(()),
         };
     }
-    println!("{} {}", Emoji("🎉", "v"), style("Done!").bright().green());
+    eprintln!("{} {}", Emoji("🎉", "v"), style("Done!").bright().green());
     Ok(())
 }
